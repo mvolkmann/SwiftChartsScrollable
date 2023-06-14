d9caf4908143458ae949ddad9388b56b8069a19f
R. Mark Volkmann <r.mark.volkmann@gmail.com>
2023-06-14 14:53:48 -0500

    Initial Commit

---
+++ SwiftChartsScrollable/SwiftChartsScrollableApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftChartsScrollableApp.swift
+//  SwiftChartsScrollable
+//
+//  Created by Mark Volkmann on 6/14/23.
+//
+
+import SwiftUI
+
+@main
+struct SwiftChartsScrollableApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
